bbd432f8f173b9df6c27db6bdb11356ca480f732
Alexander Wu <alexander.wu7@gmail.com>
2020-03-17 20:59:28 -0700

    Reorganize code structure to submit

--- data/prepare_test_video.py
+++ data/prepare_test_video.py
@@ -1,66 +1,81 @@
 # Get keypoint data from the images and prepare for pix2pix
 from utils import *
-import os
 
-inputFolder = 'images/'
-outputFolderKp = 'new_data/'
-saveFilename = outputFolderKp + 'kp_test.pickle'
-outputFolderForpix2pix = 'pix2pix_input/'
-inputToA2KeyModel = 'new_data/images/'
+parser = argparse.ArgumentParser()
+parser.add_argument('dataset', help='dataset to process (obama or trump)')
+parser.add_argument('--jaw', help='whether use jaw keypoints')
 
-cmd = 'rm -rf ' + inputFolder + '*-square-x-100.jpeg'
-subprocess.call(cmd, shell=True)
 
-if not os.path.exists(outputFolderForpix2pix):
-	# Create directory
-	subprocess.call('mkdir -p ' + outputFolderForpix2pix, shell=True)
-if not os.path.exists(outputFolderKp):
-	# Create directory
-	subprocess.call('mkdir -p ' + outputFolderKp, shell=True)
-if not os.path.exists(inputToA2KeyModel):
-	# Create directory
-	subprocess.call('mkdir -p ' + inputToA2KeyModel, shell=True)
+if __name__ == '__main__':
 
-searchNames = inputFolder + '*/*' + '.jpeg'
-filenames = sorted(glob(searchNames, recursive=True))
+    args = parser.parse_args()
 
-d = []
-for file in tqdm(filenames):
-    #print(file)
-    try:
-        img = cv2.imread(file)
-        x = int(np.floor((img.shape[1]-256)/2))
-        # Crop to a square image
-        crop_img = img
-        outputName = file
+    kp_str = 'jaw' if args.jaw else 'lip'
 
-        # extract the keypoints
-        keypoints = get_facial_landmarks(outputName)
-        l = getKeypointFeatures(keypoints)
-        unit_kp, N, tilt, mean = l[0], l[1], l[2], l[3]
-        kp_mouth = unit_kp[48:68]
-        kp_jaw = unit_kp[0:17]
-        store_list = [kp_mouth, N, tilt, mean, unit_kp, keypoints]
-        d.append(store_list)
+    inputFolder = args.dataset + '/images/'
+    outputFolderKp = args.dataset + '/kp_{}/'.format(kp_str)
+    saveFilename = outputFolderKp + 'kp_test.pickle'
+    outputFolderForpix2pix = args.dataset + '/pix2pix_input_{}/'.format(kp_str)
+    inputToA2KeyModel = args.dataset + '/kp_{}/images/'.format(kp_str)
 
-        # create a patch based on the tilt, mean and the size of face
-        mean_x, mean_y = int(mean[0]), int(mean[1])
-        size = int(N/10)
-        aspect_ratio_mouth = 1.8
-        # print('mean (y, x):', mean_y, mean_x, 'size:', size)
-        patch_img = crop_img.copy()
-        patch_img[ mean_y-int(0.5*size): mean_y+int(1.2*size), mean_x-int(aspect_ratio_mouth*size):
-                  mean_x+int(0.85*aspect_ratio_mouth*size) ] = 0
-        cv2.imwrite(inputToA2KeyModel + file[len(inputFolder):-len('/00001.jpeg')]+file[len(inputFolder)+len('00001-000/'):-len('.jpeg')] + '.png', patch_img)
-        drawLips(keypoints, patch_img)
-        drawJaws(keypoints, patch_img)
+    if not os.path.exists(outputFolderForpix2pix):
+    	# Create directory
+    	subprocess.call('mkdir -p ' + outputFolderForpix2pix, shell=True)
+    if not os.path.exists(outputFolderKp):
+    	# Create directory
+    	subprocess.call('mkdir -p ' + outputFolderKp, shell=True)
+    if not os.path.exists(inputToA2KeyModel):
+    	# Create directory
+    	subprocess.call('mkdir -p ' + inputToA2KeyModel, shell=True)
 
-        # Slap the other original image onto this
-        patch_img = np.hstack((patch_img, crop_img))
-        outputNamePatch = outputFolderForpix2pix + file[len(inputFolder):-len('/00001.jpeg')]+file[len(inputFolder)+len('00001-000/'):-len('.jpeg')] + '.png'
-        cv2.imwrite(outputNamePatch, patch_img)
-    except:
-        pass
-# save the extracted keypoints
-with open(saveFilename, "wb") as output_file:
-	pkl.dump(d, output_file)
+    searchNames = inputFolder + '*/*' + '.jpeg'
+    filenames = sorted(glob(searchNames, recursive=True))
+
+    d = []
+    for file in tqdm(filenames):
+        #print(file)
+        try:
+            img = cv2.imread(file)
+            x = int(np.floor((img.shape[1]-256)/2))
+            # Crop to a square image
+            crop_img = img
+            outputName = file
+
+            # extract the keypoints
+            keypoints = get_facial_landmarks(outputName)
+            l = getKeypointFeatures(keypoints)
+            unit_kp, N, tilt, mean = l[0], l[1], l[2], l[3]
+            kp_mouth = unit_kp[48:68]
+            kp_jaw = unit_kp[0:17]
+            store_list = [kp_mouth, N, tilt, mean, unit_kp, keypoints]
+            d.append(store_list)
+
+            # create a patch based on the tilt, mean and the size of face
+            mean_x, mean_y = int(mean[0]), int(mean[1])
+            if args.jaw:
+                size = int(N/10)
+            else:
+                size = int(N/15)
+            aspect_ratio_mouth = 1.8
+            # print('mean (y, x):', mean_y, mean_x, 'size:', size)
+            patch_img = crop_img.copy()
+            if args.jaw:
+                patch_img[mean_y-int(0.5*size): mean_y+int(1.2*size), mean_x-int(aspect_ratio_mouth*size):
+                          mean_x+int(0.85*aspect_ratio_mouth*size)] = 0
+            else:
+                patch_img[mean_y-size: mean_y+size, mean_x-int(aspect_ratio_mouth*size):
+                          mean_x+int(aspect_ratio_mouth*size)] = 0
+            cv2.imwrite(inputToA2KeyModel + file[len(inputFolder):-len('/00001.jpeg')]+file[len(inputFolder)+len('00001-000/'):-len('.jpeg')] + '.png', patch_img)
+            drawLips(keypoints, patch_img)
+            if args.jaw:
+                drawJaws(keypoints, patch_img)
+
+            # Slap the other original image onto this
+            patch_img = np.hstack((patch_img, crop_img))
+            outputNamePatch = outputFolderForpix2pix + file[len(inputFolder):-len('/00001.jpeg')]+file[len(inputFolder)+len('00001-000/'):-len('.jpeg')] + '.png'
+            cv2.imwrite(outputNamePatch, patch_img)
+        except:
+            pass
+    # save the extracted keypoints
+    with open(saveFilename, "wb") as output_file:
+    	pkl.dump(d, output_file)
--- data/processing.py
+++ data/processing.py
@@ -4,30 +4,35 @@ np.seterr(divide='ignore', invalid='ignore')
 EPSILON = 1e-8
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-trim", "--trim", type=bool, default=False)
-parser.add_argument("-extract_images", "--extract_images", type=bool, default=False)
-parser.add_argument("-extract_audio", "--extract_audio", type=bool, default=False)
-parser.add_argument("-extract_image_kp", "--extract_image_kp", type=bool, default=False)
-parser.add_argument("-extract_pca", "--extract_pca", type=bool, default=False)
-parser.add_argument("-extract_audio_kp", "--extract_audio_kp", type=bool, default=False)
+parser.add_argument('dataset', help='dataset to process (obama or trump)')
+parser.add_argument('--trim')
+parser.add_argument('--extract_images')
+parser.add_argument('--extract_audio')
+parser.add_argument('--extract_image_kp')
+parser.add_argument('--extract_pca')
+parser.add_argument('--extract_audio_kp')
+
 
 if __name__ == '__main__':
 
 	args = parser.parse_args()
-	
-	if (args.trim):
+	dataset = args.dataset
 
-		inputFolder = 'videos/'
-		outputFolder = 'trimmed_videos/'
+	################################################################################
+	if args.trim:
+
+		inputFolder = dataset + '/videos/'
+		outputFolder = dataset + '/trimmed_videos/'
+
 		video_width = 456
 
-		if not(os.path.exists(outputFolder)):
+		if not os.path.exists(outputFolder):
 			# Create directory
 			subprocess.call('mkdir -p ' + outputFolder, shell=True)
 
-		for i in range(2, 21):
-			num = str(i).rjust(5, '0')
-			captions_filename = 'captions/'+ num +'.en.vtt'
+		for i in range(1, 11):
+			num = str(i).zfill(5)
+			captions_filename = '{}/captions/{}.en.vtt'.format(dataset, num)
 			inputfilename = inputFolder + num + '.mp4 '
 			captions = WebVTT().read(captions_filename)
 
@@ -39,15 +44,16 @@ if __name__ == '__main__':
 				end = caption.end
 				t = int(round(get_sec(end) - get_sec(ss)))
 				print('index: ', idx, 'Text: ', caption.text, 'Time: ', t)
-				cmd = 'ffmpeg -i ' + inputfilename + '-vf scale='+ str(video_width) + ':256 ' + '-ss ' + str(ss) + ' -t ' + str(t) + ' -acodec copy ' + outputfilename
+				cmd = 'ffmpeg -i {} -vf scale={}:256 -ss {} -t {} -acodec copy {}'.format(inputfilename, video_width, ss, t, outputfilename)
 				subprocess.call(cmd, shell=True)
 
-	if (args.extract_images):
+	################################################################################
+	if args.extract_images:
 
-		inputFolder = 'trimmed_videos/'
-		outputFolder = 'images/'
+		inputFolder = dataset + '/trimmed_videos/'
+		outputFolder = dataset + '/images/'
 
-		if not(os.path.exists(outputFolder)):
+		if not os.path.exists(outputFolder):
 			# Create directory
 			subprocess.call('mkdir -p ' + outputFolder, shell=True)
 
@@ -59,7 +65,7 @@ if __name__ == '__main__':
 			num = filename[len(inputFolder):-len('.mp4')]
 			print('Num: ', num)
 			# Create this directory if it doesn't exist
-			if not(os.path.exists(outputFolder+num)):
+			if not os.path.exists(outputFolder+num):
 				# Create directory
 				subprocess.call('mkdir -p ' + outputFolder+num, shell=True)
 
@@ -78,12 +84,13 @@ if __name__ == '__main__':
 
 			subprocess.call('rm -rf '+ outputFolder + num + '/*.bmp', shell=True)
 
-	if (args.extract_audio):
+	################################################################################
+	if args.extract_audio:
 
-		inputFolder = 'trimmed_videos/'
-		outputFolder = 'audios/'
+		inputFolder = dataset + '/trimmed_videos/'
+		outputFolder = dataset + '/audios/'
 
-		if not(os.path.exists(outputFolder)):
+		if not os.path.exists(outputFolder):
 			# Create directory
 			subprocess.call('mkdir -p ' + outputFolder, shell=True)
 
@@ -93,13 +100,14 @@ if __name__ == '__main__':
 			cmd = 'ffmpeg -i ' + file + ' -ab 160k -ac 1 -ar 16000 -vn ' + outputFolder + file[len(inputFolder): -len('.mp4')] + '.wav'
 			subprocess.call(cmd, shell=True)
 
-	if (args.extract_image_kp):
+	################################################################################
+	if args.extract_image_kp:
 
-		inputFolder = 'images/'
-		outputFolder = 'image_kp_raw/'
+		inputFolder = dataset + '/images/'
+		outputFolder = dataset + '/image_kp_raw/'
 		resumeFrom = 0
 
-		if not(os.path.exists(outputFolder)):
+		if not os.path.exists(outputFolder):
 			# Create directory
 			subprocess.call('mkdir -p ' + outputFolder, shell=True)
 
@@ -111,9 +119,10 @@ if __name__ == '__main__':
 			key = directory[len(inputFolder):-1]
 			imglist = sorted(glob(directory+'*.jpeg'))
 			big_list = []
+			prev_store_list = [] # Alex: no face present in jpeg
 			for file in tqdm(imglist):
 				keypoints = get_facial_landmarks(file)
-				if not (keypoints.shape[0] == 1): # if there are some kp then
+				if not keypoints.shape[0] == 1: # if there are some kp thn
 					l = getKeypointFeatures(keypoints)
 					unit_kp, N, tilt, mean = l[0], l[1], l[2], l[3]
 					kp_mouth = unit_kp[48:68]
@@ -127,30 +136,31 @@ if __name__ == '__main__':
 			saveFilename = outputFolder + 'kp' + str(idx+resumeFrom+1) + '.pickle'
 			oldSaveFilename = outputFolder + 'kp' + str(idx+resumeFrom-2) + '.pickle'
 
-			if not (os.path.exists(saveFilename)):
-				with open(saveFilename, "wb") as output_file:
+			if not os.path.exists(saveFilename):
+				with open(saveFilename, 'wb') as output_file:
 					pkl.dump(d, output_file)
-					print('Saved output for ', (idx+resumeFrom+1), ' file.')
+					print('Saved output for ', idx+resumeFrom+1, ' file.')
 			else:
 				# Resume
-				with open(saveFilename, "rb") as output_file:
+				with open(saveFilename, 'rb') as output_file:
 					d = pkl.load(output_file)
-					print('Loaded output for ', (idx+resumeFrom+1), ' file.')
+					print('Loaded output for ', idx+resumeFrom+1, ' file.')
 
 			# Keep removing stale versions of the files
-			if (os.path.exists(oldSaveFilename)):
+			if os.path.exists(oldSaveFilename):
 				cmd = 'rm -rf ' + oldSaveFilename
 				subprocess.call(cmd, shell=True)
 
-	if (args.extract_audio_kp):
+	################################################################################
+	if args.extract_audio_kp:
 
-		inputFolder = 'audios/'
-		outputFolder = 'audio_kp/'
+		inputFolder = dataset + '/audios/'
+		outputFolder = dataset + '/audio_kp/'
 		resumeFrom = 0
 		frame_rate = 5
 		kp_type = 'mel'
 
-		if not(os.path.exists(outputFolder)):
+		if not os.path.exists(outputFolder):
 			# Create directory
 			subprocess.call('mkdir -p ' + outputFolder, shell=True)
 
@@ -161,7 +171,7 @@ if __name__ == '__main__':
 		for idx, file in enumerate(tqdm(filelist[resumeFrom:])):
 			key = file[len(inputFolder):-len('.wav')]
 
-			if(kp_type == 'world'):
+			if kp_type == 'world':
 				x, fs = sf.read(file)
 				# 2-1 Without F0 refinement
 				f0, t = pw.dio(x, fs, f0_floor=50.0, f0_ceil=600.0,
@@ -172,7 +182,7 @@ if __name__ == '__main__':
 				ap = pw.d4c(x, f0, t, fs)
 				features = np.hstack((f0.reshape((-1, 1)), np.hstack((sp, ap))))
 
-			elif (kp_type == 'mel'):
+			elif kp_type == 'mel':
 				(rate, sig) = wav.read(file)
 				features = logfbank(sig,rate)
 
@@ -181,38 +191,41 @@ if __name__ == '__main__':
 			saveFilename = outputFolder + 'audio_kp' + str(idx+resumeFrom+1) + '_' + kp_type + '.pickle'
 			oldSaveFilename = outputFolder + 'audio_kp' + str(idx+resumeFrom-2) + '_' + kp_type + '.pickle'
 
-			if not (os.path.exists(saveFilename)):
-				with open(saveFilename, "wb") as output_file:
+			if not os.path.exists(saveFilename):
+				with open(saveFilename, 'wb') as output_file:
 					pkl.dump(d, output_file)
-					# print('Saved output for', (idx+resumeFrom+1), 'file.')
+					# print('Saved output for', idx+resumeFrom+1, 'file.')
 			else:
 				# Resume
-				with open(saveFilename, "rb") as output_file:
+				with open(saveFilename, 'rb') as output_file:
 					d = pkl.load(output_file)
-					print('Loaded output for ', (idx+resumeFrom+1), ' file.')
+					print('Loaded output for ', idx+resumeFrom+1, ' file.')
 
 			# Keep removing stale versions of the files
-			if (os.path.exists(oldSaveFilename)):
+			if os.path.exists(oldSaveFilename):
 				cmd = 'rm -rf ' + oldSaveFilename
 				subprocess.call(cmd, shell=True)
 
 		print('Saved Everything')
 
-	if (args.extract_pca):
+	################################################################################
+	if args.extract_pca:
 
-		inputFolder = 'image_kp_raw/'
-		outputFolder = 'pca/'
-# 		numOfFiles = 1467 # First 20 videos
-		numOfFiles = 139 # cause we are only doing 2 videos right now
+		inputFolder = dataset + '/image_kp_raw/'
+		outputFolder = dataset + '/pca/'
+		numOfFiles = 333 # First 5 obama videos
+		# numOfFiles = 1414 # First obama 20 videos
+		# numOfFiles = 250 # First 5 trump videos
+		#numOfFiles = 137 # First 2 videos
 		new_list = []
 
 		filename = inputFolder + 'kp' + str(numOfFiles) + '.pickle'
 
-		if not(os.path.exists(outputFolder)):
+		if not os.path.exists(outputFolder):
 			# Create directory
 			subprocess.call('mkdir -p ' + outputFolder, shell=True)
 
-		if (os.path.exists(filename)):
+		if os.path.exists(filename):
 			with open(filename, 'rb') as file:
 				big_list = pkl.load(file)
 			print('Keypoints file loaded')
@@ -222,13 +235,24 @@ if __name__ == '__main__':
 
 		print('Unwrapping all items from the big list')
 
+		key_skipped = 0#
+		frame_kp_skipped = 0#
 		for key in tqdm(sorted(big_list.keys())):
+			if len(big_list[key]) == 0:
+				print('key:', key)
+				key_skipped += 1
 			for frame_kp in big_list[key]:
+				if len(frame_kp) == 0:
+					frame_kp_skipped += 1
+					continue
 				kp_mouth = frame_kp[0]
 				x = kp_mouth[:, 0].reshape((1, -1))
 				y = kp_mouth[:, 1].reshape((1, -1))
 				X = np.hstack((x, y)).reshape((-1)).tolist()
 				new_list.append(X)
+
+		print('key_skipped:', key_skipped) #
+		print('frame_kp_skipped:', frame_kp_skipped) #
 
 		X = np.array(new_list)
 
@@ -236,7 +260,7 @@ if __name__ == '__main__':
 		pca.fit(X)
 		with open(outputFolder + 'pca' + str(numOfFiles) + '.pickle', 'wb') as file:
 			pkl.dump(pca, file)
-	
+
 		with open(outputFolder + 'explanation' + str(numOfFiles) + '.pickle', 'wb') as file:
 			pkl.dump(pca.explained_variance_ratio_, file)
 
@@ -246,15 +270,18 @@ if __name__ == '__main__':
 		print('Upsampling...')
 
 		# Upsample the lip keypoints
+		skipped = 0  # Alex: keep track of invalid frames
 		upsampled_kp = {}
 		for key in tqdm(sorted(big_list.keys())):
 			# print('Key:', key)
 			nFrames = len(big_list[key])
+			if len(big_list[key]) == 0 or len(big_list[key][0]) == 0:
+				skipped += 1
+				continue
 			factor = int(np.ceil(100/29.97))
 			# Create the matrix
 			new_unit_kp = np.zeros((int(factor*nFrames), big_list[key][0][0].shape[0], big_list[key][0][0].shape[1]))
 			new_kp = np.zeros((int(factor*nFrames), big_list[key][0][-1].shape[0], big_list[key][0][-1].shape[1]))
-
 			# print('Shape of new_unit_kp:', new_unit_kp.shape, 'new_kp:', new_kp.shape)
 
 			for idx, frame in enumerate(big_list[key]):
@@ -262,7 +289,7 @@ if __name__ == '__main__':
 				new_kp[(idx*(factor)), :, :] = frame[-1]
 				new_unit_kp[(idx*(factor)), :, :] = frame[0]
 
-				if (idx > 0):
+				if idx > 0:
 					start = (idx-1)*factor + 1
 					end = idx*factor
 					for j in range(start, end):
@@ -271,8 +298,9 @@ if __name__ == '__main__':
 						l = getKeypointFeatures(new_kp[j, :, :])
 						# print('')
 						new_unit_kp[j, :, :] = l[0][48:68, :]
-			
+
 			upsampled_kp[key] = new_unit_kp
+		print('skipped:', skipped)
 
 		# Use PCA to de-correlate the points
 		d = {}
